8c0121fd791d3ce3d06e4340de145c857ca4c2ab
Mauricio Melo <mauriciomelo.contato@outlook.com>
2018-10-22 14:09:04 -0300

    Criação do projeto

---
+++ src/main/java/com/newbank/autocredenciamento/AutocredenciamentoApplication.java
@@ -0,0 +1,12 @@
+package com.newbank.autocredenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocredenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocredenciamentoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/newbank/autocredenciamento/AutocredenciamentoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newbank.autocredenciamento;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutocredenciamentoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
